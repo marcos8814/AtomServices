8ca06eb4712385ea83a4ecca74b5218d167d8783
marcos8814 <marcos.svmota@gmail.com>
2020-09-15 20:26:16 -0400

    traduzindo arquivo app.js para o portugues

--- application/views/usuarios/index.php
+++ application/views/usuarios/index.php
@@ -34,7 +34,7 @@
                       <th>Usuário</th>
                       <th>Login</th>
                       <th>Ativo</th>
-                      <th class="text-right">Ações</th>
+                      <th class="text-right no-sort">Ações</th>
                     </tr>
                   </thead>
                  
