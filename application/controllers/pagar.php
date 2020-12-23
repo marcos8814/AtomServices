6aa2e476356d17f3c7eae3ab2aa7ebda914d0688
marcos8814 <marcos.svmota@gmail.com>
2020-12-22 21:46:41 -0400

    Editando contas a pagar parte 1

--- application/controllers/pagar.php
+++ application/controllers/pagar.php
@@ -43,5 +43,40 @@
         	     $this->load->view('layout/footer');	
        }
 
+       public function edit($conta_pagar_id=NULL)
+       {
+         if (!$conta_pagar_id || !$this->core_model->get_by_id('contas_pagar',array('conta_pagar_id'=>$conta_pagar_id))) {
+            $this->session->set_flashdata('erro', 'Conta não encotrada ');
+            redirect('pagar');
+         }else{
+                $data = array(
+          
+               'titulo'=>'Contas a pagar cadastrados',
+
+                'styles'=> array(
+                'vendor/select2/select2.min.css',
+               ),
+
+                'scripts'=> array(
+                'vendor/mask/jquery.mask.min.js',
+                'vendor/mask/app.js',
+                'vendor/select2/select2.min.js',
+                'vendor/select2/app.js',
+                ),
+    
+                'conta_pagar'=> $this->core_model->get_by_id('contas_pagar',array('conta_pagar_id'=>$conta_pagar_id)), 
+                'fornecedores' => $this->core_model->get_all('fornecedores'),
+              );
+                 //echo '<pre>';
+                 //print_r($data['contas_pagar']);
+                 //exit();
+
+               $this->load->view('layout/header', $data); 
+               $this->load->view('pagar/edit');  
+               $this->load->view('layout/footer');
+
+         }
+       }
+
      }
 ?>
--- application/views/pagar/edit.php
+++ application/views/pagar/edit.php
@@ -14,7 +14,7 @@
 
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-      <li class="breadcrumb-item"><a href="<?php echo base_url('produtos'); ?>">produtos</a></li>
+      <li class="breadcrumb-item"><a href="<?php echo base_url('pagar'); ?>">Contas a pagar</a></li>
       <li class="breadcrumb-item active" aria-current="page"><?php  echo $titulo; ?></li>
     </ol>
   </nav>
@@ -25,139 +25,69 @@
     <div class="card-body">
      <!--back end  -->
      <form class="user" method="POST" name="form_edit"> <!--metodo post reponsavel por proteger campo da url -->
-      <p><strong><i class="fas fa-tools"></i>&nbsp;&nbsp;Útima Alteração: </strong><?php echo formata_data_banco_com_hora( $produto->produto_data_alteracao);?></p>:
+      <p><strong><i class="fas fa-tools"></i>&nbsp;&nbsp;Útima Alteração: </strong><?php echo formata_data_banco_com_hora( $conta_pagar->conta_pagar_data_alteracao);?></p>:
 
       <!--O codigo abaixo moldura dos dados pessoais   -->
       <fieldset class="mt-4 border p-2">          
        <!--O codigo abaixo se refere os iconis dos dados pessoais -->
-       <legend class="font-small"><i class="fab fa-product-hunt"></i>&nbsp;Dados do produto</legend>
+       <legend class="font-small"><i class="fas fa-money-bill-alt"></i>&nbsp;Dados da conta</legend>
 
-
-       <div class="form-group row"> <!--altera o tamanho do campo nome  -->
-         <div class="col-md-2"> 
-           <label>Código do produto</label>
-           <input type="text" class="form-control form-control-user" name="produto_codigo"  value="<?php echo $produto->produto_codigo; ?>" readonly = ""> 
-         </div>
-
-         <div class="col-md-10"> 
-           <label>Descrição do produto</label>
-           <input type="text" class="form-control form-control-user" name="produto_descricao" placeholder="Descrição do produto" value="<?php echo $produto->produto_descricao; ?>"> <!--last_name é o sobrenome do campo da tabela que esta no banco de dados  -->
-           <?php echo form_error('produto_descricao','<small class="form-text text-danger">','</small>'); ?>
-           <!-- O codigo verifica se o campo obirgatorio esta preechido-->
-
-         </div>
-      </div>
-          <div class="form-group row"> 
-          <div class="col-md-3 mb-3 "> 
-           <label>Marca</label>
-           <select class="custom-select" name="produto_marca_id">
-            <?php foreach ($marcas as $marca):?>
-              <option  value="<?php echo $marca->marca_id ?>"<?php echo ($marca->marca_id == $produto->produto_marca_id ? 'selected' : '' ) ?>><?php echo $marca->marca_nome; ?></option>
-            <?php endforeach; ?>
-          </select>
-        </div>
+        <div class="form-group row"> 
         
-        <!-- O codigo abaixo busca na tabela categoria os campos categoria nome e tras para o formularia da tabela produtos-->
-        <div class="col-md-3 mb-2 "> 
-         <label>Categoria</label>
-         <select class="custom-select" name="produto_categoria_id">
-          <?php foreach ($categorias as $categoria):?>
-            <option  value="<?php echo $categoria->categoria_id ?>"<?php echo ($categoria->categoria_id == $produto->produto_categoria_id ? 'selected' : '' ) ?>><?php echo $categoria->categoria_nome; ?></option>
-          <?php endforeach; ?>
-        </select>
-      </div>
-      <!-- O codigo abaixo busca na tabela categoria os campos categoria nome e tras para o formularia da tabela produtos-->
-      <div class="col-md-3 mb-2 "> 
+      <div class="col-md-6 mb-2 "> 
        <label>Fornecedor</label>
-       <select class="custom-select" name="produto_fornecedor_id">
+       <select class="custom-select contas_pagar"  name="conta_pagar_fornecedor_id">
         <?php foreach ($fornecedores as $fornecedor):?>
-          <option  value="<?php echo $fornecedor->fornecedor_id ?>"<?php echo ($fornecedor->fornecedor_id == $produto->produto_fornecedor_id ? 'selected' : '' ) ?>><?php echo $fornecedor->fornecedor_nome_fantasia; ?></option>
+          <option  value="<?php echo $fornecedor->fornecedor_id ?>"<?php echo ($fornecedor->fornecedor_id == $conta_pagar->conta_pagar_fornecedor_id ? 'selected' : '' ) ?>><?php echo $fornecedor->fornecedor_nome_fantasia; ?></option>
         <?php endforeach; ?>
       </select>
+      <?php echo form_error('conta_pagar_fornecedor_id','<small class="form-text text-danger">','</small>'); ?>
     </div>
 
-    <div class="col-md-3"> 
-     <label>Unidade do produto</label>
-     <input type="text" class="form-control form-control-user" name="produto_unidade" placeholder="Unidade do produto" value="<?php echo $produto->produto_unidade; ?>"> <!--last_name é o sobrenome do campo da tabela que esta no banco de dados  -->
-     <?php echo form_error('produto_unidade','<small class="form-text text-danger">','</small>'); ?>
-     <!-- O codigo verifica se o campo obirgatorio esta preechido-->
-
-   </div>
-          
-  </div>
-       
-      
-      
-  </fieldset>
-  <fieldset class="mt-4 border p-2">          
- <!--O codigo abaixo se refere os iconis dos dados pessoais -->
- <legend class="font-small"><i class="fas fa-funnel-dollar"></i></i>&nbsp;Precificação e estoque</legend>
-
-
- <div class="form-group row mb-3"> 
-   <div class="col-md-3"> 
-     <label>Preço de custo</label>
-     <input type="text" class="form-control form-control-user money" name=" produto_preco_custo" placeholder="Preço de custo" value="<?php echo $produto->produto_preco_custo; ?>"> <!--last_name é o sobrenome do campo da tabela que esta no banco de dados  -->
-     <?php echo form_error('produto_preco_custo','<small class="form-text text-danger">','</small>'); ?>
+     <div class="col-md-2"> 
+     <label>Data de vencimento</label>
+     <input type="date" class="form-control form-control-user-date" name="conta_pagar_data_vencimento"  value="<?php echo $conta_pagar->conta_pagar_data_vencimento; ?>"> <!--last_name é o sobrenome do campo da tabela que esta no banco de dados  -->
+     <?php echo form_error('conta_pagar_data_vencimento','<small class="form-text text-danger">','</small>'); ?>
      <!-- O codigo verifica se o campo obirgatorio esta preechido-->
 
    </div>
 
-   <div class="col-md-3"> 
-     <label>Preço venda</label>
-     <input type="text" class="form-control form-control-user money" name=" produto_preco_venda" placeholder="Preço de venda" value="<?php echo $produto->produto_preco_venda; ?>"> <!--last_name é o sobrenome do campo da tabela que esta no banco de dados  -->
-     <?php echo form_error('produto_preco_venda','<small class="form-text text-danger">','</small>'); ?>
+    <div class="col-md-2"> 
+     <label>Valor da conta</label>
+     <input type="text" class="form-control form-control-user-date money2" name="conta_pagar_valor"  value="<?php echo $conta_pagar->conta_pagar_valor; ?>"> <!--last_name é o sobrenome do campo da tabela que esta no banco de dados  -->
+     <?php echo form_error('conta_pagar_valor','<small class="form-text text-danger">','</small>'); ?>
      <!-- O codigo verifica se o campo obirgatorio esta preechido-->
 
    </div>
 
-   <div class="col-md-3"> 
-     <label>Estoque minimo</label>
-     <input type="text" class="form-control form-control-user " name="produto_estoque_minimo" placeholder="Estoque minimo" value="<?php echo $produto->produto_estoque_minimo; ?>"> <!--last_name é o sobrenome do campo da tabela que esta no banco de dados  -->
-     <?php echo form_error('    produto_estoque_minimo','<small class="form-text text-danger">','</small>'); ?>
-     <!-- O codigo verifica se o campo obirgatorio esta preechido-->
-
-   </div>
-
-   <div class="col-md-3"> 
-     <label>Quantidade de produto em estoque </label>
-     <input type="text" class="form-control form-control-user " name=" produto_qtde_estoque" placeholder="Quantidade em estoque" value="<?php echo $produto->produto_qtde_estoque; ?>"> <!--last_name é o sobrenome do campo da tabela que esta no banco de dados  -->
-     <?php echo form_error('  produto_qtde_estoque','<small class="form-text text-danger">','</small>'); ?>
-     <!-- O codigo verifica se o campo obirgatorio esta preechido-->
-
-   </div>
-
-
-
- </div>
-
- <div class="form-group row mb-3"> 
+    
 
    <div class="col-md-2"> 
-     <label>Produto ativo</label>
+     <label>Status da conta</label>
 
-     <select class="custom-select" name="produto_ativo">
-       <option value="0"<?php echo ($produto->produto_ativo == 0) ?'selected' :'' ?>>Não</option>
+     <select class="custom-select" name="conta_pagar_status">
+       <option value="1"<?php echo ($conta_pagar->conta_pagar_status == 1) ?'selected' :'' ?>>Paga</option>
        <!-- o codigo acima verifica no banco de dados se a opção esta ativo ou não -->
-       <option value="1"<?php echo ($produto->produto_ativo == 1) ?'selected' :'' ?>>Sim</option>
+       <option value="0"<?php echo ($conta_pagar->conta_pagar_status == 0) ?'selected' :'' ?>>Pendente</option>
        <!-- o codigo acima verifica no banco de dados se a opção esta ativo ou não -->
      </select>   
 
    </div>
-
-    <div class="col-md-10"> 
-       <label>Descrição do produto*</label>
-       <textarea class="form-control form-control-user " name="produto_obs"><?php echo $produto->produto_obs; ?></textarea> <!--email é o nome do campo da tabela que esta no banco de dados  -->
-       <?php echo form_error('produto_obs','<small class="form-text text-danger">','</small>'); ?>
+    <div class="col-md-12"> 
+       <label>Observação</label>
+       <textarea class="form-control " name="conta_pagar_obs"><?php echo $conta_pagar->conta_pagar_obs; ?></textarea> <!--email é o nome do campo da tabela que esta no banco de dados  -->
+       <?php echo form_error('conta_pagar_obs','<small class="form-text text-danger">','</small>'); ?>
        <!-- O codigo verifica se o campo obirgatorio esta preechido-->
      </div>
 
- </div> 
-
-</fieldset>
+       
+      
+      
+  </fieldset>
+ 
 
  <div class="form-group row">  
-   <input type="hidden" name="produto_id" value="<?php echo $produto->produto_id;?>"/>
+   <input type="hidden" name="conta_pagar_id" value="<?php echo $conta_pagar->conta_pagar_id;?>"/>
  </div> 
 
  <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
